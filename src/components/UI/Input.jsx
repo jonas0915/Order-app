d09e495628beac3ede7fd51523e16016435c2f59
Jonas Penaso <edciljonas15@yahoo.com>
2022-04-28 19:15:11 -0700

    2nd commit

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -3,9 +3,12 @@ import classes from "./Input.module.css";
 
 const Input = (props) => {
   return (
-    <div>
+    <div className={classes.input}>
+      {/* prop id from input obeject is targeted and will be passed on the spread operator*/}
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input />
+      {/* spreadOperator ensures all key value pairs are added  */}
+      {/* as props to input */}
+      <input {...props.input} />
     </div>
   );
 };
