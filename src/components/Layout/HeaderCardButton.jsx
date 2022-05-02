a85acbfff93c38fd422f85dcbd3b3e3c73bb7ede
Jonas Penaso <edciljonas15@yahoo.com>
2022-05-02 15:26:43 -0700

    add button animation

--- src/components/Layout/HeaderCardButton.jsx
+++ src/components/Layout/HeaderCardButton.jsx
@@ -1,19 +1,46 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import CartIcon from "../Cart/CartIcon.jsx";
 import CartContext from "../../Store/cart-context.jsx";
 import classes from "./HeaderCardButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   // Contex
   const cartCtx = useContext(CartContext);
+  // Object Destructing to pull-out items off the cart
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  // Animation
+  const btnClasses = `${classes.button}  ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    // if this is true, it will just retur & the rest of the fnctn won't execute
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    // Resets animation after 300ms after adding an item to the cart
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // Clean-up function = cleansup the timer to be reused
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     // props.onClick is passed from Header.jsx
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
