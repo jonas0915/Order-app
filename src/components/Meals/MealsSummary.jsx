29b1936928f7074ea7c20a35bbe165c80d351581
Jonas Penaso <edciljonas15@yahoo.com>
2022-05-04 08:53:49 -0700

    intro edit

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -6,7 +6,7 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
-        Choose your favorite meal from our broad serction of available meals and
+        Choose your favorite meal from our broad section of available meals and
         enjoy a delicious lunch or dinner at home.
       </p>
       <p>
